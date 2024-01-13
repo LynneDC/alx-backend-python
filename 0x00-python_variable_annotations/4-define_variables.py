3670993fc65cdf338ff57ddacef3032340a918d8
LynneDC <roselinedc1308@gmail.com>
2024-01-13 11:36:43 +0200

    define variables with values in annotation

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Define and annotate variables with specific values
+a is an integer with value of 1
+pi is a float with value of 3.14
+i_understand_annotations is a boolean with value of True
+school is a string with value of "Holberton"
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
