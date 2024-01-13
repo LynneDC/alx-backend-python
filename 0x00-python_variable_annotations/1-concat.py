587e934213b8890311e171dc01bec0993b8ad68d
LynneDC <roselinedc1308@gmail.com>
2024-01-13 10:29:33 +0200

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+typed annotated funtion that concat two strings
+it takes a string and a string as args
+return the concatenation of its values as a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings"""
+    return str1 + str2
