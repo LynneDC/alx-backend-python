1b5a6f1557710f9c5511b8e6762e72812f7753ab
LynneDC <roselinedc1308@gmail.com>
2024-01-13 12:17:15 +0200

    add union to solve number of args error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,8 +4,9 @@ typed annotated funtion that takes
 a list of intergers and floats as an arg
 and return their sum as a float
 """
-from typing import List
+from typing import List, Union
 
-def sum_mixed_list(mxd_list: List[int, float]) -> float:
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Return the sum of a list of numbers"""
     return sum(mxd_list)
