73857e3da3f1039ee35ea58f9c3c881840d0dfe7
LynneDC <roselinedc1308@gmail.com>
2024-01-13 09:45:59 +0200

    typed annotation that takes in 2 float args and return sum as a float

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 """
-typed annotated funtion that 
+typed annotated funtion that
 takes a float and a float as args
 return the sum of its values as afloat
 """
 
+
 def add(a: float, b: float) -> float:
+    """Return the sum of a and b."""
     return a + b
