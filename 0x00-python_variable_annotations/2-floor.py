0f9ae176394a526859b5bc0e4b3bdf98a170ef33
LynneDC <roselinedc1308@gmail.com>
2024-01-13 10:36:54 +0200

    takes a float and return a floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+typed annotated funtion that takes a float n as arg
+return the floor of the float
+"""
+
+
+def floor(n: float) -> float:
+    """Return the floor of a number"""
+    return n // 1 
