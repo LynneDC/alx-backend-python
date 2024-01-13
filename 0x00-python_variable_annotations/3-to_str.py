eafcc72a3ccf0af782049f021582a47a6928483f
LynneDC <roselinedc1308@gmail.com>
2024-01-13 11:24:16 +0200

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+typed annotated funtion that takes a float n as arg
+return the string representation of the float
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a number"""
+    return str(n)
