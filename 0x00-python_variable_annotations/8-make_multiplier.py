301fc6dd89dc3f22a8deaa8d9a60a830b92793e5
LynneDC <roselinedc1308@gmail.com>
2024-01-13 12:49:16 +0200

    use collable to call a function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+typed annotated funtion that takes a float multiplier as arg
+return a function that multiply a float by multiplier
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiply a float by multiplier"""
+    def multiply(n: float) -> float:
+        """Return the multiplication of a number and a multiplier"""
+        return n * multiplier
+    return multiply
