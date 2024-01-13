9535195629f026b396646ce11400037daa99cfff
LynneDC <roselinedc1308@gmail.com>
2024-01-13 11:50:09 +0200

    function that takes in a float[list] and return sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+typed annotated function that takes a list of floats as arg
+return the sum of the list
+"""
+import math
+
+def sum_list(input_list: list) -> float:
+    """Return the sum of a list of numbers"""
+    return sum(input_list) 
