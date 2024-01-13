bbac2f96d26bb4f2d17254e50ff77ccb406222ac
LynneDC <roselinedc1308@gmail.com>
2024-01-13 09:58:52 +0000

    using tyipng

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,8 @@
 typed annotated function that takes a list of floats as arg
 return the sum of the list
 """
-import math
+from typing  import List
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of numbers"""
     return sum(input_list) 
