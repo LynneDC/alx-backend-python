85d7373ecf4ba70ab69a4d37d623bd13a96872e7
LynneDC <roselinedc1308@gmail.com>
2024-01-13 10:03:12 +0000

    fix pycode sty;el errer

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,9 @@
 typed annotated function that takes a list of floats as arg
 return the sum of the list
 """
-from typing  import List
+from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of numbers"""
-    return sum(input_list) 
+    return sum(input_list)
