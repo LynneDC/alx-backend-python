41b4c87be716b5d4c3a891a4254eb82ba6875120
LynneDC <roselinedc1308@gmail.com>
2024-01-16 01:01:38 +0000

    fix module import error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ use time module to measure time elapse
 import time
 import asyncio
 
-wait_n = __import__("1-concurrent_coroutine").wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
