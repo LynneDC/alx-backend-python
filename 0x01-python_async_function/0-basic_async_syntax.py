b6acf02f2a5c6219f4a42a8e1d1b7718118a9058
LynneDC <roselinedc1308@gmail.com>
2024-01-16 00:39:17 +0000

    fix pycode

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 Asynchronous coroutine that takes in an int arg (max_delay)
-max_delay has default value = 10  
+max_delay has default value = 10
 asynchronous coroutine name is wait_random
-it waits for a random delay between 0 and max_delay seconds (float value) 
+it waits for a random delay between 0 and max_delay seconds (float value)
 and eventually returns it
 """
 import random
@@ -14,4 +14,4 @@ async def wait_random(max_delay: int = 10) -> float:
     """Wait randomly between 0 and max_delay seconds."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return delay 
+    return delay
