4a2b651ddc60e493aae5ea0d94e0ca3674b54d85
LynneDC <roselinedc1308@gmail.com>
2024-02-01 23:43:18 +0000

    return coroutine and loops 10 times

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
 """
 coroutine called <code>async_generator</code>
 takes no arguments
-loops 10 times, 
-each time asynchronously wait 1 second, 
+loops 10 times,
+each time asynchronously wait 1 second,
 then yield a random number between 0 and 10
 using the random module
 """
 import asyncio
 import random
+from typing import Generator
 
-""" defining a function that takesno arguments and returns a coroutine """
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
+    """ defining a function that takes no arguments
+    and returns a coroutine """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
